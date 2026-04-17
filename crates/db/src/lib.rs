01917bc4c0bc229939a1c4cc16cd0924ed653ab1
subh <subhajitchaudhury05@gmail.com>
2026-04-17 13:00:46 +0200

    feat : db connection established

---
+++ crates/db/src/lib.rs
@@ -0,0 +1,31 @@
+use sqlx::{postgres::PgPoolOptions, PgPool};
+
+pub async fn connection_pool(database_url: &str) -> Result<PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .max_connections(10)
+        .connect(database_url)
+        .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::env::var;
+
+    #[tokio::test]
+    async fn test_database_connection() {
+        dotenvy::dotenv().ok();
+        let database_url =
+            var("DATABASE_URL").expect("CRITICAL DATABASE_URL must be set in .env or system");
+        let pool = connection_pool(&database_url)
+            .await
+            .expect("failed to connect to DB");
+
+        let row: (i32,) = sqlx::query_as("SELECT 1")
+            .fetch_one(&pool)
+            .await
+            .expect("Failed to execute Select 1");
+        assert_eq!(row.0, 1);
+        println!("Database connection successful");
+    }
+}
