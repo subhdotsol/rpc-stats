5cd22d074c66148d77c5c00c47bd47a1f51a7d64
subh <subhajitchaudhury05@gmail.com>
2026-04-17 13:17:03 +0200

    feat : api spinned up

--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpResponse, HttpServer, Responder, get};
+use tracing::info;
+use tracing_subscriber::EnvFilter;
+
+#[get("/health")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok().body("OK")
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Welcome to RPC STATS")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    // Initialize logging
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env().add_directive(tracing::Level::INFO.into()))
+        .init();
+
+    let host = "127.0.0.1";
+    let port = 8080;
+
+    info!(
+        "Starting Actix RPC Stats server at http://{}:{}",
+        host, port
+    );
+
+    HttpServer::new(|| App::new().service(index).service(health_check))
+        .bind((host, port))?
+        .run()
+        .await
 }
